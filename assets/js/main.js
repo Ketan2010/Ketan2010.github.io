7fdecbe2c046fb6a8340d17f2646adf96a2a902f
Ketan Patil <k2p1020@gmail.com>
2021-02-18 14:37:56 +0530

    V1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,19 +55,19 @@ sr.reveal('.home__social-icon',{ interval: 200});
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{});
 sr.reveal('.about__subtitle',{delay: 200});
-sr.reveal('.about__text',{delay: 200});
+sr.reveal('.about__text',{delay: 200}); 
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{});
+/*sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{});
 sr.reveal('.skills__data',{interval: 200});
-sr.reveal('.hab__img',{delay: 200});
+sr.reveal('.hab__img',{delay: 200});  */
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200});
+/*sr.reveal('.work__img',{interval: 200}); */
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200});
+/* sr.reveal('.contact__input',{interval: 200});*/
 
 
 /*BACK TO TOP BUTTON*/
